1084f9a4cb22f6ebb36134c3ad6f3067d2efaad8
Yuyang Wang <stevenyuyang@gmail.com>
2017-12-21 01:02:27 -0500

    tags field is a textarea now

--- app/components/ACME/create/create.directive.js
+++ app/components/ACME/create/create.directive.js
@@ -1,6 +1,19 @@
-angular.module('app')
-    .controller('createController', createController);
+(function () {
+    angular.module('app')
+        .controller('createController', createController)
+        .directive('createPage', createPage);
 
-function createController() {
-    
-}
+    function createController() {
+        var vm = this;
+        // denotes the visibility of each step
+        vm.stepVisibility = [true, false, false, false];
+    }
+
+    function createPage() {
+        return {
+            restrict: 'E',
+            templateUrl: 'app/components/ACME/create/create.html',
+            controller: 'createController as create'
+        }
+    }
+})();
